0012c61f6bfbd05559384d767e2f0e42a72458b4
Georgi Georgiev <georgi.georgiev.vn@gmail.com>
2023-07-28 18:47:10 +0300

    Selection Sort

---
+++ array/sort/selection-sort.js
@@ -0,0 +1,47 @@
+/**
+ * Selection Sort
+ * 
+ * Time Complexity: O(n^2)
+ * Memory Complexity: O(1)
+ * 
+ * @param {number[]} arr - Array of numbers
+ * @returns {number[]} - Sorted array
+ */
+const fn = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        // Re/set minimal index
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        // Swap minimal element with current element
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+
+    return arr;
+}
+
+const tests = [
+    [
+        [1, 3, 2], [1, 2, 3]
+    ],
+    [
+        [5, 3, 2, 4, 1], [1, 2, 3, 4, 5]
+    ],
+    [
+        [1, 6, 5, 4, 3, 9, 7, 8, 2 , 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    ]
+];
+
+for (const i in tests) {
+    const [unsorted, sorted] = tests[i];
+    const unsortedCopy = JSON.parse(JSON.stringify(unsorted));
+    const result = fn(unsorted);
+    if (result.toString() === sorted.toString()) {
+        console.log(`${unsortedCopy} => ${result}`)
+    }
+    console.assert(result.toString() === sorted.toString(), 
+        `${unsorted} => Expected: ${sorted} Actual: ${result}`
+    )
+}
